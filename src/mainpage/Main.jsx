fd65bb22cd6609792a4d38c43ae09a544c44e46e
hoyeol0513 <hoyeol0513@naver.com>
2022-09-02 13:23:52 +0900

    main backImg, Service introduction

--- src/mainpage/Main.jsx
+++ src/mainpage/Main.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import MetaTag from "../components/MetaTag";
-import { TextArea } from "@adobe/react-spectrum";
 import Wording from "../components/Wording";
 import "bootstrap/js/src/collapse.js";
 
@@ -9,9 +8,9 @@ const Main = () => {
   return (
     <>
       <MetaTag />
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary bg-opacity-75">
         <div className="container px-5">
-          <a className="navbar-brand" href="#!">
+          <a className="navbar-brand fs-4 fw-bold" href="#!">
             NAGA
           </a>
           <button
@@ -28,23 +27,13 @@ const Main = () => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#!">
-                  Home
+                <a className="nav-link active text-dark fw-bold" aria-current="page" href="#!">
+                  Login
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#!">
-                  About
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#!">
-                  Contact
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#!">
-                  Services
+                <a className="nav-link text-dark fw-bold" href="#!">
+                  Sign Up
                 </a>
               </li>
             </ul>
@@ -57,18 +46,14 @@ const Main = () => {
           <div className="row gx-5 justify-content-center">
             <div className="col-lg-8">
               <div className="text-center my-5">
-                <h1 className="display-10 fw-bolder text-white mb-4">
-                  여행지를 선택해주세요.
+                <h1 className="display-10 fw-bolder text-white mb-5">
+                  여행지를 선택해주세요
                 </h1>
-                <p className="lead text-white-50 mb-4">
-                  {/* Quickly design and customize responsive mobile-first sites
-                  with Bootstrap, the world’s most popular front-end open source
-                  toolkit! */}
-                </p>
+                
                 <div className="d-grid gap-3 d-sm-flex justify-content-sm-center">
                   <a
                     className="btn btn-primary btn-lg px-4 me-sm-5"
-                    href="#features"
+                    href="#!"
                   >
                     해외
                   </a>
@@ -86,46 +71,41 @@ const Main = () => {
         <div className="container px-5 my-5 ">
           <div className="row gx-5">
             <div className="col-lg-4 mb-5 mb-lg-0">
-              <h2 className="h2 fw-bolder">위시리스트</h2>
-              <p className="h5">
-                The real voyage of discovery consists not in seeking new
-                landscapes but in having new eyes.
+              <h2 className="h4 fw-bolder">위시리스트</h2>
+              <p>
+                원하는 관광지 및 숙소, 맛집, 렌트카 등을 사용자가 직접 선택하고 관리할 수 있는 시스템을 제공합니다.
               </p>
-              <p className="text-decoration-none text">
-                -마르셀 푸르스트(Marcel Proust)-
-              </p>
+              <a className="text-decoration-none" href="https://www.op.gg/summoners/kr/%ED%9B%88%EC%88%98%EC%A1%B0%EA%B8%88%EB%A7%8C" target="_blank" rel="noreferrer">
+                훈수조금만 주세요.... 저 브론즈임...
+                <i className="bi bi-arrow-right"></i>
+              </a>  
             </div>
             <div className="col-lg-4 mb-5 mb-lg-0">
               <h2 className="h4 fw-bolder">365일 휴무</h2>
               <p>
                 저희 사이트는 365일 휴무이므로 문의를 보내지 마시길 바랍니다.
               </p>
-              <a className="text-decoration-none" href="#!">
-                보내지마세요
+              <a className="text-decoration-none" href="https://www.epeople.go.kr/nep/pttn/pttnOnln/PttnOnlnWrtnInfo.npaid" target="_blank" rel="noreferrer">
+                보내면 신고함 ㅅㄱ
                 <i className="bi bi-arrow-right"></i>
               </a>
             </div>
             <div className="col-lg-4">
-              <h2 className="h4 fw-bolder">매일 개같이 유기한다.</h2>
+              <h2 className="h4 fw-bolder">방문자</h2>
               <p>
-                Paragraph of text beneath the heading to explain the heading.
-                We'll add onto it with another sentence and probably just keep
-                going until we run out of words.
+                방문자 수 google 애널리스틱 이용해서 보여주면 될 것 같아요.
               </p>
-              <a className="text-decoration-none" href="#!">
-                Call to action
-                <i className="bi bi-arrow-right"></i>
-              </a>
+              
             </div>
           </div>
         </div>
       </section>
 
       {/*  */}
-      <section className="py-2 border-bottom">
+      <section className="py-2">
         <div className="container px-5 my-2 px-5">
           <div className="text-center mb-4">
-            <h2 className="fw-bolder fs-1">오늘의 여행 글귀</h2>
+            <h2 className="fw-bolder fs-2 mt-4">오늘의 여행 띵언</h2>
             <p className="lead mb-0"></p>
           </div>
           <div className="row gx-5 justify-content-center">
